15b3078c55b6a5849bf8d9fce9de453650f86356
cs-john-lennon <john.lennon@concrete.com.br>
2019-03-23 22:45:12 -0300

    adiciona intro activity

---
+++ app/src/main/kotlin/com/lennon/navigationapp/view/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.lennon.navigationapp.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.lennon.navigationapp.R
+
+class IntroActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.intro_activity)
+    }
+}
