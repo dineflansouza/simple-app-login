c1533e26537d662f5c16a69a8bc9178722df63bd
cs-john-lennon <john.lennon@concrete.com.br>
2019-03-24 10:17:35 -0300

    remove androidx

--- app/src/main/kotlin/com/lennon/navigationapp/view/IntroActivity.kt
+++ app/src/main/kotlin/com/lennon/navigationapp/view/IntroActivity.kt
@@ -1,10 +1,10 @@
 package com.lennon.navigationapp.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.support.v7.app.AppCompatActivity
 import com.lennon.navigationapp.R
 
-class IntroActivity: AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
